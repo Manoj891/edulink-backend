d4d616796b2f3fc632bcf70d852eee4bca6ce7bd
manoj.mahato <kamalmanoj9@gmail.com>
2025-08-14 14:23:47 +0545

    Dynamic month fee

--- src/main/java/com/ms/ware/online/solution/school/service/utility/OrganizationMasterServiceImp.java
+++ src/main/java/com/ms/ware/online/solution/school/service/utility/OrganizationMasterServiceImp.java
@@ -3,28 +3,27 @@
 package com.ms.ware.online.solution.school.service.utility;
 
 
+import com.ms.ware.online.solution.school.config.Message;
 import com.ms.ware.online.solution.school.config.security.AuthenticatedUser;
 import com.ms.ware.online.solution.school.config.security.AuthenticationFacade;
 import com.ms.ware.online.solution.school.dao.utility.OrganizationMasterDao;
 import com.ms.ware.online.solution.school.entity.utility.OrganizationMaster;
-import com.ms.ware.online.solution.school.config.Message;
-
-import java.io.File;
-import javax.servlet.http.HttpServletRequest;
-
-import com.ms.ware.online.solution.school.exception.CustomException;
 import com.ms.ware.online.solution.school.model.DatabaseName;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+
 @Service
+@Slf4j
 public class OrganizationMasterServiceImp implements OrganizationMasterService {
 
     @Autowired
-    OrganizationMasterDao da;
-    Message message = new Message();
-    String msg = "";
+    private OrganizationMasterDao da;
+    @Autowired
+    private Message message;
     int row;
     @Autowired
     private AuthenticationFacade facade;
@@ -43,7 +42,7 @@ public class OrganizationMasterServiceImp implements OrganizationMasterService {
         try {
             obj.setId(1l);
             row = da.save(obj);
-            msg = da.getMsg();
+            String msg = da.getMsg();
             if (row > 0) {
                 return message.respondWithMessage("Success");
             } else if (msg.contains("Duplicate entry")) {
@@ -59,52 +58,54 @@ public class OrganizationMasterServiceImp implements OrganizationMasterService {
 
     @Override
     public String logo(MultipartFile logo, MultipartFile idCardLogo, MultipartFile principleSignature, MultipartFile billBackground) {
+
+
+        String location = message.getFilepath(DatabaseName.getDocumentUrl());
+        File f = new File(location + "/Organization");
         try {
-            String location = message.getFilepath(DatabaseName.getDocumentUrl());
-            File f = new File(location + "/Organization");
-            try {
-                if (!f.exists()) {
-                    f.mkdirs();
-                }
-            } catch (Exception e) {
-                return message.respondWithError(e.getMessage());
+            if (!f.exists()) {
+                f.mkdirs();
             }
-            try {
-                if (billBackground != null && billBackground.getSize() > 1) {
-                    f = new File(location + "/Organization/bill-background.png");
-                    billBackground.transferTo(f);
-                }
-            } catch (Exception ex) {
-               return message.respondWithError(ex.getMessage());
-            }
-            try {
-                if (idCardLogo != null && idCardLogo.getSize() > 1) {
-                    f = new File(location + "/Organization/idCardLogo.png");
-                    idCardLogo.transferTo(f);
-                }
-            } catch (Exception ex) {
-               return message.respondWithError(ex.getMessage());
-            }
-            try {
-                if (idCardLogo != null && idCardLogo.getSize() > 1) {
-                    f = new File(location + "/Organization/principleSignature.png");
-                    principleSignature.transferTo(f);
-                }
-            } catch (Exception ex) {
-               return message.respondWithError(ex.getMessage());
-            }
-            try {
-                if (idCardLogo != null && idCardLogo.getSize() > 1) {
-                    f = new File(location + "/Organization/Logo.png");
-                    logo.transferTo(f);
-                }
-            } catch (Exception ex) {
-               return message.respondWithError(ex.getMessage());
-            }
-            return message.respondWithMessage("Success");
         } catch (Exception e) {
-           return message.respondWithError(e.getMessage());
+            return message.respondWithError(e.getMessage());
         }
+        try {
+            if (billBackground != null && billBackground.getSize() > 1) {
+                f = new File(location + "/Organization/bill-background.png");
+                billBackground.transferTo(f);
+                log.info("Bill background image has been uploaded");
+            }
+        } catch (Exception ex) {
+            return message.respondWithError(ex.getMessage());
+        }
+        try {
+            if (idCardLogo != null && idCardLogo.getSize() > 1) {
+                f = new File(location + "/Organization/idCardLogo.png");
+                idCardLogo.transferTo(f);
+                log.info("ID Card logo image has been uploaded");
+            }
+        } catch (Exception ex) {
+            return message.respondWithError(ex.getMessage());
+        }
+        try {
+            if (principleSignature != null && principleSignature.getSize() > 1) {
+                principleSignature.transferTo(new File(location + "/Organization/principleSignature.png"));
+                log.info("Principle signature image has been uploaded");
+            }
+        } catch (Exception ex) {
+            return message.respondWithError(ex.getMessage());
+        }
+        try {
+            if (logo != null && logo.getSize() > 1) {
+                f = new File(location + "/Organization/Logo.png");
+                logo.transferTo(f);
+                log.info("Logo image has been uploaded");
+            }
+        } catch (Exception ex) {
+            return message.respondWithError(ex.getMessage());
+        }
+        return message.respondWithMessage("Success");
+
     }
 
 }
