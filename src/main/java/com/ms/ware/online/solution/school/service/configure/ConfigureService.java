8e9cdbac107ca461c2de8d90d8bcea2c26b8ca0b
manoj.mahato <kamalmanoj9@gmail.com>
2025-09-06 10:24:55 +0545

    hostel dynamic

--- src/main/java/com/ms/ware/online/solution/school/service/configure/ConfigureService.java
+++ src/main/java/com/ms/ware/online/solution/school/service/configure/ConfigureService.java
@@ -1,14 +1,10 @@
 package com.ms.ware.online.solution.school.service.configure;
 
 public interface ConfigureService {
-    void functionConfigure();
-
-    void configureDistrictMunicipality();
 
     void configure();
 
     void function();
-
 
     String districtConfig();
 
